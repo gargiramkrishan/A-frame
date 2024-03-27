961eba73db14cbefdee5af2e70b97fc349ae0fba
gargiramkrishan <71713523+gargiramkrishan@users.noreply.github.com>
2024-03-27 14:43:19 +0530

    Add files via upload

---
+++ New folder (9)/car.js
@@ -0,0 +1,9 @@
+// Registering component in log-component.js
+AFRAME.registerComponent('car', {
+    schema: {
+      message: {type: 'string', default: 'Hello, World!'}
+    },
+    init: function () {
+        console.log(this.data.message);
+      }
+  });
